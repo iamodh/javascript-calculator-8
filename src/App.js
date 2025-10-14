092236d23cfa279cf3c17ed4975ebee5e4a9b82e
Donghwan Oh <imodh97@gmail.com>
2025-10-14 22:00:00 +0900

    feat(app): handle exceptions for erros
    Add exception handling for predictable error cases:
    1. when the input is empty
    2. when the prefix for a custom separator is invalid
    3. when the input has characters that cannot be converted to a number, except for separators

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 class App {
   async run(input) {
+    // 예외처리 1. input이 빈 문자열일때
+    if (input.length === 0) {
+      console.error('입력 형식이 유효하지 않습니다. 입력이 비어있습니다.');
+      return;
+    }
     const separator = [':', ','];
     let preprocessed = input;
 
@@ -7,19 +12,31 @@ class App {
     if (preprocessed.startsWith('//')) {
       const [prefix, origin] = input.split('\n');
       const customSeparator = prefix.slice(2);
+
+      // 예외처리 1. prefix 형식이 유효하지 않을 때 (\n을 두 번 입력했을 때 또는 separator 하나 이외의 다른 문자가 포함되어 있을 때)
+      if (origin.length === 0 || customSeparator.length > 1) {
+        console.error(
+          '입력 형식이 유효하지 않습니다. 커스텀 구분자 추가 규칙을 확인해주세요.'
+        );
+        return;
+      }
       separator.push(customSeparator);
       preprocessed = origin;
-    } else {
-      // 예외처리 1. prefix 형식이 유효하지 않을 때
     }
 
-    // 예외처리 2. prefix에 두개 이상의 custom seperator가 있을 때
+    // 예외처리 2. preprocessed에 숫자와 seperator(':'와 ',') 이외의 문자가 있을 때
+    for (let i = 0; i < preprocessed.length; i++) {
+      if (separator.includes(preprocessed[i])) {
+        continue;
+      } else if (isNaN(Number(preprocessed[i]))) {
+        console.error(
+          '입력 형식이 유효하지 않습니다. 숫자로 변환할 수 없는 문자가 포함되어 있습니다.'
+        );
+        return;
+      }
+    }
 
-    // 예외처리 3. origin에 ':'와 ',' 이외의 separator가 있을 때
-
-    // 예외처리 4. origin에 separator 이외에 숫자로 변환할 수 없는 문자가 있을 때
-
-    //  for loop를 돌며 구분자에 따라 splits로 분리하는 작업
+    // for loop를 돌며 구분자에 따라 splits로 분리하는 작업
     // 모든 구분자를 ,로 replace하는 방식으로 리팩토링 계획
     for (let i = 0; i < separator.length; i++) {
       const stringToSplit = String(preprocessed);
