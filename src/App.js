2055c663fd3636c8736e4af96e55a22883d56362
Donghwan Oh <imodh97@gmail.com>
2025-10-14 20:17:04 +0900

    fix(app): correct misspellings
    Change the misspelled word 'seperator' to 'separator' to improve code readability and maintainability.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 class App {
   async run(input) {
     console.log(input);
-    const seperators = [':', ','];
+    const separator = [':', ','];
+
     // custom seperator 확인
     const [prefix, origin] = input.split('\n');
     if (prefix.startsWith('//')) {
-      const customSeperator = prefix.slice(2);
-      seperators.push(customSeperator);
+      const customSeparator = prefix.slice(2);
+      separator.push(customSeparator);
     }
 
     let splits = origin;
     //  for loop를 돌며 구분자에 따라 splits로 분리하는 작업
-    for (let i = 0; i < seperators.length; i++) {
+    // 모든 구분자를 ,로 replace하는 방식으로 리팩토링 계획
+    for (let i = 0; i < separator.length; i++) {
       const stringToSplit = String(splits);
-      splits = stringToSplit.split(seperators[i]);
+      splits = stringToSplit.split(separator[i]);
     }
 
     // splits 배열 flat (원소를 하나로 줄임)
