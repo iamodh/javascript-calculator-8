3a135c0fa958d4211fcb337e1b45c2fbc9cc724b
Donghwan Oh <imodh97@gmail.com>
2025-10-14 17:01:35 +0900

    feat(app): implement calculate feature using fixed separators

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 class App {
-  async run() {}
+  async run(input) {
+    const seperators = [':', ','];
+    let splits = input;
+
+    //  for loop를 돌며 구분자에 따라 splits로 분리하는 작업
+    for (let i = 0; i < seperators.length; i++) {
+      const stringToSplit = String(splits);
+      splits = stringToSplit.split(seperators[i]);
+    }
+
+    // 분리된 결과에서 합을 구함
+    let sum = 0;
+    for (let i = 0; i < splits.length; i++) {
+      sum += Number(splits[i]);
+    }
+
+    console.log(sum);
+
+    return sum;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import App from "./App.js";
+import App from './App.js';
 
 const app = new App();
-await app.run();
+await app.run('1,2:3');
