8df4e74d628095e54d394e396470c1ae5d11e9fe
Donghwan Oh <imodh97@gmail.com>
2025-10-14 20:41:17 +0900

    fix(app): make calculator work when input doesn't have prefix for custom separator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 class App {
   async run(input) {
-    console.log(input);
     const separator = [':', ','];
+    let preprocessed = input;
 
     // custom seperator 확인
-    const [prefix, origin] = input.split('\n');
-    if (prefix.startsWith('//')) {
+    if (preprocessed.startsWith('//')) {
+      const [prefix, origin] = input.split('\n');
       const customSeparator = prefix.slice(2);
       separator.push(customSeparator);
+      preprocessed = origin;
+    } else {
+      // 예외처리 1. prefix 형식이 유효하지 않을 때
     }
 
-    let splits = origin;
+    // 예외처리 2. prefix에 두개 이상의 custom seperator가 있을 때
+
+    // 예외처리 3. origin에 ':'와 ',' 이외의 separator가 있을 때
+
+    // 예외처리 4. origin에 separator 이외에 숫자로 변환할 수 없는 문자가 있을 때
+
     //  for loop를 돌며 구분자에 따라 splits로 분리하는 작업
     // 모든 구분자를 ,로 replace하는 방식으로 리팩토링 계획
     for (let i = 0; i < separator.length; i++) {
-      const stringToSplit = String(splits);
-      splits = stringToSplit.split(separator[i]);
+      const stringToSplit = String(preprocessed);
+      preprocessed = stringToSplit.split(separator[i]);
     }
 
     // splits 배열 flat (원소를 하나로 줄임)
-    splits = String(splits).split(',');
+    const splits = String(preprocessed).split(',');
 
     // 분리된 결과에서 합을 구함
     let sum = 0;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import App from './App.js';
 
 const app = new App();
-await app.run('//!\n1,2:3!5!7');
+await app.run('//!\n1,2:3!4');
