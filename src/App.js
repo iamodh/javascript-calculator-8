93d6053457ac07df935dc447eb29d53daa09e6bb
Donghwan Oh <imodh97@gmail.com>
2025-10-16 23:58:57 +0900

    refactor(app): simplify split logic using regular expression
    rename 'base' to 'stringToParse' for clarify
    simplify summing logic using reduce

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App {
       '덧셈할 문자열을 입력해 주세요.\n'
     );
 
-    // 예외처리 1. input이 빈 문자열일때
+    // input이 빈 문자열일때
     if (input.length === 0) {
       throw Error(
         '[ERROR] 입력 형식이 유효하지 않습니다. 입력이 비어있습니다.'
@@ -15,7 +15,7 @@ class App {
     }
 
     const separators = [':', ','];
-    let base;
+    let stringToParse;
 
     // custom seperator 확인
     if (input.startsWith('//')) {
@@ -39,38 +39,36 @@ class App {
       }
 
       separators.push(customSeparator);
-      base = right;
+      stringToParse = right;
     } else {
-      base = input;
+      stringToParse = input;
     }
 
-    // seperators 이외의 문자가 있는지 확인
-    for (let i = 0; i < base.length; i++) {
-      if (separators.includes(base[i])) {
-        continue;
-      } else if (isNaN(base[i])) {
+    // 정규표현식에 사용되는 문자를 이스케이프 ([ => \[)
+    const escapedSeparators = separators.map((s) =>
+      s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+    );
+
+    // base를 separator 안에 있는 문자로 배열로 분리한다.
+    const regexp = new RegExp(escapedSeparators.join('|'));
+    const parsed = stringToParse.split(regexp);
+
+    const numbers = parsed.map((c) => {
+      const number = Number(c);
+      if (isNaN(number)) {
         throw Error(
           '[ERROR] 입력에 숫자로 변환할 수 없는 문자가 포함되어 있습니다.'
         );
       }
-    }
 
-    // for loop를 돌며 구분자를 기준으로 파싱
-    for (let i = 0; i < separators.length; i++) {
-      const stringToParse = String(base);
-      base = stringToParse.split(separators[i]);
-    }
+      if (number < 0) {
+        throw Error('[ERROR] 입력에 음수가 포함되어 있습니다.');
+      }
 
-    // base 배열에서 숫자만 분리
-    const splits = String(base).split(',');
+      return number;
+    });
 
-    // 분리된 결과에서 합을 구함
-    let sum = 0;
-    for (let i = 0; i < splits.length; i++) {
-      sum += Number(splits[i]);
-    }
-
-    MissionUtils.Console.print(`결과 : ${sum}`);
+    const sum = numbers.reduce((acc, cur) => acc + cur, 0);
 
     Console.print(`결과 : ${sum}`);
   }
