163c5c59cac167cde880e769139043e34de1d94f
Donghwan Oh <imodh97@gmail.com>
2025-10-16 20:56:11 +0900

    refactor(app): change variable names
    - separator -> separators
    - preprocessed -> base
    - prefix, origin -> left, right
    - stringToSplit -> stringToParse

--- src/App.js
+++ src/App.js
@@ -9,49 +9,60 @@ class App {
 
     // 예외처리 1. input이 빈 문자열일때
     if (input.length === 0) {
-      //   console.error('입력 형식이 유효하지 않습니다. 입력이 비어있습니다.');
-      throw Error('[ERROR]');
+      throw Error(
+        '[ERROR] 입력 형식이 유효하지 않습니다. 입력이 비어있습니다.'
+      );
     }
-    const separator = [':', ','];
-    let preprocessed = input;
+
+    const separators = [':', ','];
+    let base;
 
     // custom seperator 확인
-    if (preprocessed.startsWith('//')) {
-      const [prefix, origin] = preprocessed.split('\\n'); // compare string \n, not newline character
-      const customSeparator = prefix.slice(2);
+    if (input.startsWith('//')) {
+      const [left, right] = input.split('\\n'); // 이스케이프 문자를 더해 문자열 \n 기준으로 분리
 
-      // 예외처리 1. prefix 형식이 유효하지 않을 때 (\n로 끝나지 않을 때 또는 \n이 두번 쓰였을 때 또는  separator 하나 이외의 다른 문자가 포함되어 있을 때)
-      if (!origin || origin.length === 0 || customSeparator.length > 1) {
-        // console.error(
-        //   '입력 형식이 유효하지 않습니다. 커스텀 구분자 추가 규칙을 확인해주세요.'
-        // );
-        throw Error('[ERROR]');
+      // \\n로 끝나지 않는 경우 (//!1)
+      if (right === undefined) {
+        throw Error('[ERROR] 커스텀 구분자 형식이 올바르지 않습니다.');
       }
-      separator.push(customSeparator);
-      preprocessed = origin;
+
+      const customSeparator = left.substring(2);
+
+      // \\n을 두 번 사용한 경우 (//!\\n\\n1)
+      if (customSeparator.length === 0) {
+        throw Error('[ERROR] 커스텀 구분자가 형식이 올바르지 않습니다.');
+      }
+
+      // 커스텀 구분자가 하나 이상인 경우 (//!@\\n1)
+      if (customSeparator.length > 1) {
+        throw Error('[ERROR] 커스텀 구분자가 형식이 올바르지 않습니다.');
+      }
+
+      separators.push(customSeparator);
+      base = right;
+    } else {
+      base = input;
     }
 
-    // 예외처리 2. preprocessed에 숫자와 seperator(':'와 ',') 이외의 문자가 있을 때
-    for (let i = 0; i < preprocessed.length; i++) {
-      if (separator.includes(preprocessed[i])) {
+    // seperators 이외의 문자가 있는지 확인
+    for (let i = 0; i < base.length; i++) {
+      if (separators.includes(base[i])) {
         continue;
-      } else if (isNaN(Number(preprocessed[i]))) {
-        // console.error(
-        //   '입력 형식이 유효하지 않습니다. 숫자로 변환할 수 없는 문자가 포함되어 있습니다.'
-        // );
-        throw Error('[ERROR]');
+      } else if (isNaN(base[i])) {
+        throw Error(
+          '[ERROR] 입력에 숫자로 변환할 수 없는 문자가 포함되어 있습니다.'
+        );
       }
     }
 
-    // for loop를 돌며 구분자에 따라 splits로 분리하는 작업
-    // 모든 구분자를 ,로 replace하는 방식으로 리팩토링 계획
-    for (let i = 0; i < separator.length; i++) {
-      const stringToSplit = String(preprocessed);
-      preprocessed = stringToSplit.split(separator[i]);
+    // for loop를 돌며 구분자를 기준으로 파싱
+    for (let i = 0; i < separators.length; i++) {
+      const stringToParse = String(base);
+      base = stringToParse.split(separators[i]);
     }
 
-    // splits 배열 flat (원소를 하나로 줄임)
-    const splits = String(preprocessed).split(',');
+    // base 배열에서 숫자만 분리
+    const splits = String(base).split(',');
 
     // 분리된 결과에서 합을 구함
     let sum = 0;
